a5950509f9c54d42bf81b84a62f2d52581316e3c
Georgina Spieth <gjspieth@gmail.com>
2024-10-23 16:08:08 +0200

    message method solved with .split and .trim

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -2,7 +2,10 @@ public class LogLevels {
 
     public static String message(String logLine) {
       // throw new UnsupportedOperationException("Please implement the (static) LogLevels.message() method");
-      return logLine.split(":")[1].trim();
+      /* Solution 1 - Using split() and trim()
+       split string at ":" & extract part at index 1
+       trim any leading/trailing spaces, eg. \r\n (carriage return and newline) */
+      //return logLine.split(":")[1].trim();
     }
 
     public static String logLevel(String logLine) {
