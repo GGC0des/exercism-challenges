8a484d4723a071d22323bb8724e3c69dc51501d5
Georgina Spieth <gjspieth@gmail.com>
2024-11-29 13:41:07 +0100

    workingItemsPerMinute method passed test

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -18,6 +18,15 @@ public class CarsAssemble {
     }
 
     public int workingItemsPerMinute(int speed) {
-       throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+      // to avoid invalid speed
+      if (speed < 0 || speed > 10) {
+        System.err.println("Error: Invalid speed for assembly line. Valid range 1 -10.");
+        return 0; // code will not run furhter once this return statement is executed
+      }
+      // runs only if speed is valid
+      double ratePerHour = productionRatePerHour(speed); // hourly production rate depending on speed
+      double ratePerMinute = ratePerHour/60; // convert to per-minute rate; value could also be declared as `int` here
+      return (int) ratePerMinute; // turnication rounding the double down
+      }
+
     }
-}
