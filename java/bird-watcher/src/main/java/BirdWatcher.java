65c61f7e3ece451180213956ab4bdf8cb9eb99ff
Georgina Spieth <gjspieth@gmail.com>
2025-01-07 14:17:26 +0100

    getLastWeek method passed test

--- java/bird-watcher/src/main/java/BirdWatcher.java
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -1,13 +1,34 @@
-
+import java.util.Arrays;
 class BirdWatcher {
     private final int[] birdsPerDay;
+    // each BirdWatcher instance signifies a week of bird watching of the last week
 
     public BirdWatcher(int[] birdsPerDay) {
         this.birdsPerDay = birdsPerDay.clone();
     }
 
+    // constructor will take argument of lastWeek[]
+    // public BirdWatcher(int[] lastWeek) {
+    //  this.birdsPerDay = lastWeek.clone();
+    //}
+
+
     public int[] getLastWeek() {
-        throw new UnsupportedOperationException("Please implement the BirdWatcher.getLastWeek() method");
+      // accessor method for birdsPerDay which according to exerc. == lastWeek[]
+
+      // Conditional to enure array has exactly 7 elements
+      if (birdsPerDay == null || birdsPerDay.length != 7) {
+        throw new IllegalArgumentException("The array must have exactly 7 elements.");
+      }
+
+      // Iteration to ensure all indices contain a valid value
+      for (int count : birdsPerDay) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("Array contains invalid values.");
+        }
+      }
+
+      return birdsPerDay;
     }
 
     public int getToday() {
