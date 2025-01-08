19cf42925d18e90128e59ff05f132e136b09adcf
Georgina Spieth <gjspieth@gmail.com>
2025-01-08 12:49:01 +0100

    turn space into underscore and kebab-case into camelCase method added

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,21 @@
 class SqueakyClean {
+  // creating a static method so that it can be called without having to create an object instance of the class
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+      if (identifier == null || identifier.isEmpty()) {
+        return identifier;
+      }
+      // replace space with underscore
+      return identifier.replace(" ","_");
+
+      // turn kebab-case to camelCase
+      String[] parts = identifier.split("-");
+      // solution with concatenation
+      String result = parts[0];
+      for (int i = 1; i < parts.length; i++) {
+        result += parts[i].substring(0, 1).toUpperCase() + parts[i].substring(1).toLowerCase();
+      }
+      return result;
+
+
     }
 }
