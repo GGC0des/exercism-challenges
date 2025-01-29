8fa2129df097d61616ccf161854f4bc45e7047c0
Georgina Spieth <gjspieth@gmail.com>
2025-01-29 17:59:35 +0100

    Tasks 1-4 passed

---
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -0,0 +1,43 @@
+class NeedForSpeed {
+  public int speed;
+  public int batteryDrain;
+  public int battery = 100;
+  private int distanceDriven = 0;
+
+  public NeedForSpeed(int speed, int batteryDrain) {
+    this.speed = speed;
+    this.batteryDrain = batteryDrain;
+  }
+
+  public boolean batteryDrained() {
+    return battery < 1 || battery < batteryDrain;
+  }
+
+    public int distanceDriven() {
+      return distanceDriven;
+    }
+
+    public void drive() {
+      if (battery >= batteryDrain) {
+        distanceDriven += this.speed;
+        battery -= this.batteryDrain;
+      }
+    }
+
+    public static NeedForSpeed nitro() {
+      return new NeedForSpeed(50, 5);
+    }
+}
+
+class RaceTrack {
+  private int distance;
+
+    RaceTrack(int distance) {
+      this.distance = distance;
+    }
+
+    public boolean canFinishRace(NeedForSpeed car) {
+      int totalBatteryDrain = (car.distanceDriven() / car.speed) * car.batteryDrain;
+      return totalBatteryDrain <= car.battery;
+    }
+}
