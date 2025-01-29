f2f7ae883b6ab1f7277287af4c6e343acb300880
Georgina Spieth <gjspieth@gmail.com>
2025-01-29 11:44:11 +0100

    Refractored code to return ternary operator directly instead of saving it to a variable that will be returned

--- java/salary-calculator/src/main/java/SalaryCalculator.java
+++ java/salary-calculator/src/main/java/SalaryCalculator.java
@@ -1,23 +1,19 @@
 public class SalaryCalculator {
     public double salaryMultiplier(int daysSkipped) {
-      double value = daysSkipped < 5 ? 1.0 : 0.85;
-      return value;
+      return daysSkipped < 5 ? 1.0 : 0.85;
     }
 
     public int bonusMultiplier(int productsSold) {
-      int value = productsSold < 20 ? 10 : 13;
-      return value;
+      return productsSold < 20 ? 10 : 13;
     }
 
     public double bonusForProductsSold(int productsSold) {
-      double value = bonusMultiplier(productsSold) * productsSold;
-      return value;
+      return bonusMultiplier(productsSold) * productsSold;
     }
 
     public double finalSalary(int daysSkipped, int productsSold) {
       double baseSalary = 1000.00;
       double salary = baseSalary * salaryMultiplier(daysSkipped) + bonusForProductsSold(productsSold);
-      double value = salary < 2000.00 ? salary : 2000.00;
-      return value;
+      return salary < 2000.00 ? salary : 2000.00;
     }
 }
