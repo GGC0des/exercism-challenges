17b1633e48219deb089c64c0c742c26bd5e9510f
Georgina Spieth <gjspieth@gmail.com>
2025-01-29 12:14:59 +0100

    .buy() method successfull

---
+++ java/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -0,0 +1,17 @@
+public class ElonsToyCar {
+    public static ElonsToyCar buy() {
+      return new ElonsToyCar();
+    }
+
+    public String distanceDisplay() {
+        throw new UnsupportedOperationException("Please implement the ElonsToyCar.distanceDisplay()  method");
+    }
+
+    public String batteryDisplay() {
+        throw new UnsupportedOperationException("Please implement the ElonsToyCar.batteryDisplay()  method");
+    }
+
+    public void drive() {
+        throw new UnsupportedOperationException("Please implement the ElonsToyCar.drive()  method");
+    }
+}
