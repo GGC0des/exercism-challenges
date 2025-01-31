4a32c2713e93f05ebf81709dfc87f4e966a480fd
Georgina Spieth <gjspieth@gmail.com>
2025-01-31 16:39:10 +0100

    print method for owner condition added. All tests passed

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -1,7 +1,13 @@
 class Badge {
     public String print(Integer id, String name, String department) {
       if (id == null) {
+        if (department == null) {
+          return name + " - OWNER";
+        }
         return String.format("%s - %s", name, department.toUpperCase());
+      }
+      if (department == null) {
+        return String.format("[%d] - %s - OWNER", id, name);
       }
       return String.format("[%d] - %s - %s", id , name, department.toUpperCase());
     }
