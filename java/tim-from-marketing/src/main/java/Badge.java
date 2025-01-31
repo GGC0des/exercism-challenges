e77b981df5ba76049ca5f2ab65ebeba1f769e621
Georgina Spieth <gjspieth@gmail.com>
2025-01-31 16:31:09 +0100

    print() method adapted to print badge for a new employee with id == null

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -1,5 +1,8 @@
 class Badge {
     public String print(Integer id, String name, String department) {
+      if (id == null) {
+        return String.format("%s - %s", name, department.toUpperCase());
+      }
       return String.format("[%d] - %s - %s", id , name, department.toUpperCase());
     }
 }
