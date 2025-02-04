4cb757abe9b96af5c9ed6c81fe3594038eabc4cf
Georgina Spieth <gjspieth@gmail.com>
2025-02-04 11:33:38 +0100

    randomness challenge: randomPlanetClass() method successfully passed all tests

--- java/captains-log/src/main/java/CaptainsLog.java
+++ java/captains-log/src/main/java/CaptainsLog.java
@@ -4,14 +4,16 @@ class CaptainsLog {
 
     private static final char[] PLANET_CLASSES = new char[]{'D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'};
 
-    private Random random;
+    private Random random; // allows to pass instance with a prediefined seed to make test results predicatable for testing
 
     CaptainsLog(Random random) {
         this.random = random;
     }
 
     char randomPlanetClass() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomPlanetClass() method");
+      char[] planetaryClasses = {'D','H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'};
+      int randomIndex = random.nextInt(planetaryClasses.length);
+      return planetaryClasses[randomIndex];
     }
 
     String randomShipRegistryNumber() {
