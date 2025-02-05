bfaba98aa648c12adb7f023377f5d3f2b5f0b607
Georgina Spieth <gjspieth@gmail.com>
2025-02-05 13:21:35 +0100

    Instance fields initialized and constructor implemented

--- java/dnd-character/src/main/java/DiceRoller.java
+++ java/dnd-character/src/main/java/DiceRoller.java
@@ -1,14 +1,32 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 class DnDCharacter {
   private final DiceRoller diceroller = new DiceRoller();
+  private int strength;
+  private int dexterity;
+  private int constitution;
+  private int intelligence;
+  private int wisdom;
+  private int charisma;
 
-    int ability(List<Integer> scores) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+  public DnDCharacter() {
+    this.strength = ability(rollDice());
+    this.dexterity = ability(rollDice());
+    this.constitution = ability(rollDice());
+    this.intelligence = ability(rollDice());
+    this.wisdom = ability(rollDice());
+    this.charisma = ability(rollDice());
+  }
+
+
+    private int ability(List<Integer> scores) {
+      scores.sort(Integer::compareTo);
+      return scores.get(1) + scores.get(2) + scores.get(3);
     }
 
-    List<Integer> rollDice() {
+    private List<Integer> rollDice() {
       return diceroller.roll4D6();
     }
 
