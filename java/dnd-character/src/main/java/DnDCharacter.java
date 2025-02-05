4fc4f42398e83f6bcf3c10ce23779fe5e674ff6a
Georgina Spieth <gjspieth@gmail.com>
2025-02-05 19:36:18 +0100

    Removed abilityName from intializeAbility line 19-24 and ability argument line 29

--- java/dnd-character/src/main/java/DnDCharacter.java
+++ java/dnd-character/src/main/java/DnDCharacter.java
@@ -16,20 +16,20 @@ class DnDCharacter {
   }
 
   private void initializeAbility() {
-    this.strength = ability("Strength", rollDice());
-    this.dexterity = ability("Dexterity", rollDice());
-    this.constitution = ability("Constitution", rollDice());
-    this.intelligence = ability("Intelligence", rollDice());
-    this.wisdom = ability("Wisdom", rollDice());
-    this.charisma = ability("Charisma", rollDice());
+    this.strength = ability(rollDice());
+    this.dexterity = ability(rollDice());
+    this.constitution = ability(rollDice());
+    this.intelligence = ability(rollDice());
+    this.wisdom = ability(rollDice());
+    this.charisma = ability(rollDice());
     this.hp = 10 + modifier(getConstitution());
   }
 
 
-    private int ability(String abilityName, List<Integer> scores) {
+    private int ability(List<Integer> scores) {
       scores.sort(Integer::compareTo);
       int sum = scores.get(1) + scores.get(2) + scores.get(3);
-      System.out.println("You rolled " + scores.get(0) + ", " + scores.get(1) + ", " + scores.get(2) + ", " + scores.get(3) + ". Your " + abilityName + " is " + sum + " (" + scores.get(1) + "+" + scores.get(2) + "+" + scores.get(3) + ").");
+      // System.out.println("You rolled " + scores.get(0) + ", " + scores.get(1) + ", " + scores.get(2) + ", " + scores.get(3) + ". Your " + abilityName + " is " + sum + " (" + scores.get(1) + "+" + scores.get(2) + "+" + scores.get(3) + ").");
       return sum;
     }
 
@@ -70,4 +70,3 @@ class DnDCharacter {
       return hp;
     }
 }
-
