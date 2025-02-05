0afa014e313729566a03a6d92560669a7d73f7ca
Georgina Spieth <gjspieth@gmail.com>
2025-02-05 19:45:18 +0100

    rollDice() and ability() are protected so they can be tested

--- java/dnd-character/src/main/java/DnDCharacter.java
+++ java/dnd-character/src/main/java/DnDCharacter.java
@@ -26,14 +26,14 @@ class DnDCharacter {
   }
 
 
-    private int ability(List<Integer> scores) {
+    protected int ability(List<Integer> scores) {
       scores.sort(Integer::compareTo);
       int sum = scores.get(1) + scores.get(2) + scores.get(3);
       // System.out.println("You rolled " + scores.get(0) + ", " + scores.get(1) + ", " + scores.get(2) + ", " + scores.get(3) + ". Your " + abilityName + " is " + sum + " (" + scores.get(1) + "+" + scores.get(2) + "+" + scores.get(3) + ").");
       return sum;
     }
 
-    private List<Integer> rollDice() {
+    protected List<Integer> rollDice() {
       return diceroller.roll4D6();
     }
 
