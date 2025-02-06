d33ff756809fda49d2738d2aecdb91f9ebb66e16
Georgina Spieth <gjspieth@gmail.com>
2025-02-06 12:58:29 +0100

    fixed bug due to variable sum missing when implementing stream in ability method, fixed in line 25,31

--- java/dnd-character/src/main/java/DnDCharacter.java
+++ java/dnd-character/src/main/java/DnDCharacter.java
@@ -22,12 +22,13 @@ class DnDCharacter {
 
 
     protected int ability(List<Integer> scores) {
+      int sum = scores.stream()                      // Convert immutable list into stream (or alternatively store in mutable new ArrayList)
+                      .sorted((a,b) -> b - a)        // Sort in descending order
+                      .limit(3)              // Use first 3 largest elements
+                      .mapToInt(Integer::intValue)   // Convert Integer objects to int primitives
+                      .sum();                        // Sum the values
       System.out.println("You rolled " + scores.get(0) + ", " + scores.get(1) + ", " + scores.get(2) + ", " + scores.get(3) + ". Your " + abilityName + " is " + sum + " (" + scores.get(1) + "+" + scores.get(2) + "+" + scores.get(3) + ").");
-      return scores.stream()                      // Convert immutable list into stream (or alternatively store in mutable new ArrayList)
-                   .sorted((a,b) -> b - a)        // Sort in descending order
-                   .limit(3)              // Use first 3 largest elements
-                   .mapToInt(Integer::intValue)   // Convert Integer objects to int primitives
-                   .sum();                        // Sum the values
+      return sum;
     }
 
 
