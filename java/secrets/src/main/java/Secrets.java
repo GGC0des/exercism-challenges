dc06fdd56a1f83deac075a2efccf3bded04493ac
Georgina Spieth <gjspieth@gmail.com>
2025-02-12 17:32:52 +0100

    Used unsigned right shift >>> to achieve logical shift to pass the test for negative numbers

--- java/secrets/src/main/java/Secrets.java
+++ java/secrets/src/main/java/Secrets.java
@@ -1,6 +1,6 @@
 public class Secrets {
     public static int shiftBack(int value, int amount) {
-        return value >> amount;
+        return value >>> amount; // unsigned right shift / logical shift that inserts 0 on the left side regardless if value was negative
     }
 
     public static int setBits(int value, int mask) {
