e311d72dca17515a41a40a637e3ffda3e097addd
Georgina Spieth <gjspieth@gmail.com>
2025-02-28 16:32:45 +0100

    enum LogLevel defined. LogLine getLogLevel() implemented

--- java/logs-logs-logs/src/main/java/LogLine.java
+++ java/logs-logs-logs/src/main/java/LogLine.java
@@ -1,10 +1,21 @@
 public class LogLine {
+    private final String logLine;
 
     public LogLine(String logLine) {
+        this.logLine = logLine;
     }
 
-    public LogLevel getLogLevel() {
-        throw new UnsupportedOperationException("Please implement the getLogLevel() method");
+    public LogLevel getLogLevel() throws IllegalAccessException {
+        String lvl = logLine.substring(1,4);
+        return switch (lvl) {
+          case "TRC" -> LogLevel.TRACE;
+          case "DBG" -> LogLevel.DEBUG;
+          case "INF" -> LogLevel.INFO;
+          case "WRN" -> LogLevel.WARNING;
+          case "ERR" -> LogLevel.ERROR;
+          case "FTL" -> LogLevel.FATAL;
+          default -> throw new IllegalAccessException("Unknown log level: " + lvl);
+        };
     }
 
     public String getOutputForShortLog() {
