d5de1448b6c1ce52085f4904d114fb7b2bd13d11
Georgina Spieth <gjspieth@gmail.com>
2025-03-10 16:49:37 +0100

    Tests passed successfully. Second Iteration submitted to Exercism.

--- java/international-calling-connoisseur/src/main/java/DialingCodes.java
+++ java/international-calling-connoisseur/src/main/java/DialingCodes.java
@@ -20,19 +20,20 @@ public class DialingCodes {
     public void addNewDialingCode(Integer code, String country) {
         if(!listOfCodes.containsKey(code) && !listOfCodes.containsValue(country)) {
             setDialingCode(code, country);
-        // throw new IllegalArgumentException broke Tests, thus commented out
+          // For real-life scenario using Excpetion to flag duplicates
+          // commented out since it would not pass the Exercism Tests otherwise
         } // else if (listOfCodes.containsKey(code)) {
-        //     throw new IllegalArgumentException("Duplicate code detected! Existing: "
-        //     + code + " -> " + getCountry(code) + ". New entry will not be added.");
-        // } else if (listOfCodes.containsValue(country)) {
-        //     Integer existingCode = listOfCodes.entrySet().stream()
-        //         .filter(entry -> entry.getValue().equals(country))
-        //         .map(Map.Entry::getKey)
-        //         .findFirst()
-        //         .orElse(null);
-        //     throw new IllegalArgumentException("Duplicate country detected! Existing: "
-        //     + existingCode + " -> " + country + ". New entry will not be added.");
-        // }
+          //     throw new IllegalArgumentException("Duplicate code detected! Existing: "
+          //     + code + " -> " + getCountry(code) + ". New entry will not be added.");
+          // } else if (listOfCodes.containsValue(country)) {
+          //     Integer existingCode = listOfCodes.entrySet().stream()
+          //         .filter(entry -> entry.getValue().equals(country))
+          //         .map(Map.Entry::getKey)
+          //         .findFirst()
+          //         .orElse(null);
+          //     throw new IllegalArgumentException("Duplicate country detected! Existing: "
+          //     + existingCode + " -> " + country + ". New entry will not be added.");
+          // }
     }
 
     public Integer findDialingCode(String country) {
