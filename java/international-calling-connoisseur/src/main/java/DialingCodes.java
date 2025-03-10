426161a410d75583d5310773039e675766902a0f
Georgina Spieth <gjspieth@gmail.com>
2025-03-10 16:00:04 +0100

    Task 1 getCodes() method implemented, tests successfull

---
+++ java/international-calling-connoisseur/src/main/java/DialingCodes.java
@@ -0,0 +1,35 @@
+import java.util.Map;
+
+public class DialingCodes {
+
+    private Map<Integer, String> dialingCodes = new HashMap<>();
+
+    public Map<Integer, String> getCodes() {
+        return dialingCodes;
+    }
+
+    public void setDialingCode(Integer code, String country) {
+        throw new UnsupportedOperationException(
+                "Delete this statement and write your own implementation.");
+    }
+
+    public String getCountry(Integer code) {
+        throw new UnsupportedOperationException(
+                "Delete this statement and write your own implementation.");
+    }
+
+    public void addNewDialingCode(Integer code, String country) {
+        throw new UnsupportedOperationException(
+                "Delete this statement and write your own implementation.");
+    }
+
+    public Integer findDialingCode(String country) {
+        throw new UnsupportedOperationException(
+                "Delete this statement and write your own implementation.");
+    }
+
+    public void updateCountryDialingCode(Integer code, String country) {
+        throw new UnsupportedOperationException(
+                "Delete this statement and write your own implementation.");
+    }
+}
