6d8ce37208e2127386e0c3f9c3109798205865dd
Georgina Spieth <gjspieth@gmail.com>
2025-03-10 16:38:39 +0100

    Task 4: addDialingCode() method implemented. IllegalArgumentException to catch duplicates commented out as Exercism Tests flag thrown Exception expecting void instead

--- java/international-calling-connoisseur/src/main/java/DialingCodes.java
+++ java/international-calling-connoisseur/src/main/java/DialingCodes.java
@@ -18,8 +18,21 @@ public class DialingCodes {
     }
 
     public void addNewDialingCode(Integer code, String country) {
-        throw new UnsupportedOperationException(
-                "Delete this statement and write your own implementation.");
+        if(!listOfCodes.containsKey(code) && !listOfCodes.containsValue(country)) {
+            setDialingCode(code, country);
+        // throw new IllegalArgumentException broke Tests, thus commented out
+        } // else if (listOfCodes.containsKey(code)) {
+        //     throw new IllegalArgumentException("Duplicate code detected! Existing: "
+        //     + code + " -> " + getCountry(code) + ". New entry will not be added.");
+        // } else if (listOfCodes.containsValue(country)) {
+        //     Integer existingCode = listOfCodes.entrySet().stream()
+        //         .filter(entry -> entry.getValue().equals(country))
+        //         .map(Map.Entry::getKey)
+        //         .findFirst()
+        //         .orElse(null);
+        //     throw new IllegalArgumentException("Duplicate country detected! Existing: "
+        //     + existingCode + " -> " + country + ". New entry will not be added.");
+        // }
     }
 
     public Integer findDialingCode(String country) {
