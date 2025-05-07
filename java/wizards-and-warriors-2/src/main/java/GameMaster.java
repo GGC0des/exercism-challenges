336be32a55bee5b9bfa81f22ef48f3a069a20a4b
Georgina Spieth <gjspieth@gmail.com>
2025-05-07 12:31:31 +0200

    describe method returing String to describe destination implemented

--- java/wizards-and-warriors-2/src/main/java/GameMaster.java
+++ java/wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -1,12 +1,14 @@
 public class GameMaster {
 
-    // TODO: define a 'describe' method that returns a description of a Character
-    public void describe (Character character) {
-        System.out.println("You're a level " + character.getLevel() + " " + character.getCharacterClass() +
-        " with " + character.getHitPoints() + " hit points.");
+    public String describe (Character character) {
+        return "You're a level " + character.getLevel() + " " + character.getCharacterClass() +
+        " with " + character.getHitPoints() + " hit points.";
     }
 
-    // TODO: define a 'describe' method that returns a description of a Destination
+    public String describe (Destination destination) {
+      return "You've arrived at " + destination.getName() + ", which has " + destination.getInhabitants()
+      + " inhabitants.";
+    }
 
     // TODO: define a 'describe' method that returns a description of a TravelMethod
 
